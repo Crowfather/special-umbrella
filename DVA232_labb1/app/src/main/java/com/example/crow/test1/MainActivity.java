673233f9de2d50b17beda61dda1f4bb7251fb7b3
ngn15002 <diet.theory@gmail.com>
2016-11-29 16:01:30 +0100

    yu
    yuyu

--- DVA232_labb1/app/src/main/java/com/example/crow/test1/MainActivity.java
+++ DVA232_labb1/app/src/main/java/com/example/crow/test1/MainActivity.java
@@ -36,11 +36,79 @@ public class MainActivity extends AppCompatActivity {
         calc_Text.append("2");
 
     }
+    public void Three_button(View view)
+    {
+        calc_Text.append("3");
+
+    }
+    public void Four_button(View view)
+    {
+        calc_Text.append("4");
+
+    }
+    public void Five_button(View view)
+    {
+        calc_Text.append("5");
+
+    }
+    public void Six_button(View view)
+    {
+        calc_Text.append("6");
+
+    }
+    public void Seven_button(View view)
+    {
+        calc_Text.append("7");
+
+    }
+    public void Eight_button(View view)
+    {
+        calc_Text.append("8");
+
+    }
+    public void Nine_button(View view)
+    {
+        calc_Text.append("9");
+
+    }
+    public void Zero_button(View view)
+    {
+        calc_Text.append("0");
+
+    }
     public void Add_button(View view)
+    {
+        if(var1 != 0){
+            var2 = Integer.parseInt(calc_Text.getText().toString());
+            var1 += var2;
+            calc_Text.setText(Integer.toString(var1));
+        }
+        else {
+            var1 = Integer.parseInt(calc_Text.getText().toString());
+            calc_Text.setText("");
+        }
+        op = '+';
+
+    }
+    public void Subtract_button(View view)
     {
         var1 = Integer.parseInt(calc_Text.getText().toString());
         calc_Text.setText("");
-        op = '+';
+        op = '-';
+
+    }
+    public void Divide_button(View view)
+    {
+        var1 = Integer.parseInt(calc_Text.getText().toString());
+        calc_Text.setText("");
+        op = '/';
+
+    }
+    public void Multiply_button(View view)
+    {
+        var1 = Integer.parseInt(calc_Text.getText().toString());
+        calc_Text.setText("");
+        op = '*';
 
     }
     public void Equal_button(View view)
@@ -53,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
         calc_Text.setText(Integer.toString(var2));
 
     }
-    public void cancel_button(View view)
+    public void Cancel_button(View view)
     {
         var1 = 0;
         var2 = 0;
